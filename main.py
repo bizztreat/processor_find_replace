b3f3949c4a7f0cfb2965882969214212bb7b24d0
Tomas Votava <tomas.votava@bizztreat.com>
2018-07-16 22:05:17 +0200

    Debug enhanced

--- main.py
+++ main.py
@@ -51,8 +51,10 @@ for table_path in csv_filelist:
 	if debug_mode: print("Processing file %s..."%table_path)
 	with open(table_path,"rt",encoding=config_encoding) as infile, open(output_path,"wt",encoding=config_encoding) as outfile:
 		lineno = 0
+		replaced = 0
 		for inline in infile:
+			replaced+=inline.count(config_find)
 			outfile.write(inline.replace(config_find,config_replacement))
 			lineno+=1
-		if debug_mode: print("Processed %d lines"%lineno)
+		if debug_mode: print("Processed %d lines, replaced %d occurrences"%(lineno,replaced))
 		
