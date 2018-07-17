1a70e51e5c72768d98785746961c533a9a130fb0
Tomas Votava <tomas.votava@bizztreat.com>
2018-07-17 11:39:50 +0200

    Release

--- main.py
+++ main.py
@@ -13,18 +13,19 @@ def find_csv_files(basedir):
 				filelist.append(os.path.join(dirpath,f))
 	return filelist
 
-def find_nth_column_position(line,delim=",\"",n=7):
-	current = 0
-	global_position = 0
-	while True:
-		pos = line.find(delim)
-		line = line[pos+len(delim):]
-		global_position+=pos+len(delim)
-		current+=1
-		if current==n-1:
-			return global_position
-		else:
-			continue
+#Single column usage
+#def find_nth_column_position(line,delim=",\"",n=7):
+#	current = 0
+#	global_position = 0
+#	while True:
+#		pos = line.find(delim)
+#		line = line[pos+len(delim):]
+#		global_position+=pos+len(delim)
+#		current+=1
+#		if current==n-1:
+#			return global_position
+#		else:
+#			continue
 		
 
 #if (not os.path.exists("/data/out/tables")): os.makedirs("/data/out/tables")
@@ -45,10 +46,11 @@ if not os.path.exists("/data/config.json"):
 	if config_find=="": config_find = "\"\"\""
 	if config_replacement=="": config_replacement = "\"\""
 	if config_encoding=="": config_encoding = "utf-8"
-	if config_tail_only=="": config_tail_only = True
-	else: config_tail_only = bool(int(config_tail_only))
-	if config_no_columns=="": config_no_columns = 7
-	else: config_no_columns = int(config_no_columns)
+	#Single column usage
+	#if config_tail_only=="": config_tail_only = True
+	#else: config_tail_only = bool(int(config_tail_only))
+	#if config_no_columns=="": config_no_columns = 7
+	#else: config_no_columns = int(config_no_columns)
 	debug_mode = 1
 else:
 	with open("/data/config.json","r") as fid:
@@ -58,8 +60,9 @@ else:
 	config_replacement = config["parameters"]["replacement"]
 	config_encoding = config["parameters"]["encoding"]
 	debug_mode = int(config["parameters"]["debug"])
-	config_tail_only = bool(int(config["parameters"]["tail_only"]))
-	config_no_columns = int(config["parameters"]["column_count"])
+	#Single column usage
+	#config_tail_only = bool(int(config["parameters"]["tail_only"]))
+	#config_no_columns = int(config["parameters"]["column_count"])
 
 if debug_mode:
 	print("Will process following tables:")
@@ -74,18 +77,18 @@ for table_path in csv_filelist:
 		lineno = 0
 		replaced_total = 0
 		for inline in infile:
-			if (config_tail_only):
-				p = find_nth_column_position(inline)
-				line_base = inline[:p-1] #till last column
-				line_tail = inline[p:-1]
-				if line_tail.endswith("\","): line_tail = line_tail[:-2]
-				else: line_tail = line_tail[:-1]
-				replaced_total+=line_tail.count(config_find)
-				line_tail = line_tail.replace(config_find,config_replacement)
-				replaced_line = line_base + "\"" + line_tail + "\"\n"
-			else:
-				replaced_total = inline.count(config_find)
-				replaced_line = inline.replace(config_find,config_replacement)
+			#Single column usage
+			#if (config_tail_only):
+			#	p = find_nth_column_position(inline)
+			#	line_base = inline[:p-1] #till last column
+			#	line_tail = inline[p:-1]
+			#	if line_tail.endswith("\","): line_tail = line_tail[:-2]
+			#	else: line_tail = line_tail[:-1]
+			#	replaced_total+=line_tail.count(config_find)
+			#	line_tail = line_tail.replace(config_find,config_replacement)
+			#	replaced_line = line_base + "\"" + line_tail + "\"\n"
+			replaced_total = inline.count(config_find)
+			replaced_line = inline.replace(config_find,config_replacement)
 			outfile.write(replaced_line)
 			lineno+=1
 		if debug_mode: print("Processed %d lines, replaced %d occurrences"%(lineno,replaced_total))
